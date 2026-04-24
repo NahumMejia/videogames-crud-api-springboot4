ce085dd38f6b2f97833555efad6f0634618b5a49
Nahum <nahummejiad@gmail.com>
2026-04-24 15:52:13 -0600

    Project added

---
+++ src/main/java/dev/nahum/videogames_crud/VideogamesCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.nahum.videogames_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamesCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nahum/videogames_crud/VideogamesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nahum.videogames_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
